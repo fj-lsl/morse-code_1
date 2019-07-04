eabe1a48750e74eb39e351ad1079498eda9d9c1d
fj-lsl <52455607+fj-lsl@users.noreply.github.com>
2019-07-04 10:07:03 +0800

    Add files via upload

---
+++ the_car.ino
@@ -0,0 +1,77 @@
+void setup() {
+
+  pinMode(5, OUTPUT);
+  pinMode(6, OUTPUT);
+  pinMode(9, OUTPUT);
+  pinMode(10, OUTPUT);
+  pinMode(11, OUTPUT);
+  pinMode(12, OUTPUT);
+  pinMode(4, OUTPUT);
+  pinMode(3, OUTPUT);
+  Serial.begin(9600);
+}
+
+int income=0;
+void loop() {
+  if(Serial.available()>0){
+    income=Serial.read();
+    switch(income){
+      case 'w':
+          forward();
+        break;
+        case 's':
+          backward();
+          break;
+        case 'd':
+          right();
+        digitalWrite(3,HIGH);
+        digitalWrite(4,LOW);
+          digitalWrite(11,LOW);
+        digitalWrite(12,LOW);
+          break;
+        case 'a':
+          left();
+        digitalWrite(11,HIGH);
+        digitalWrite(12,LOW);
+          digitalWrite(3,LOW);
+        digitalWrite(4,LOW);
+          break;
+        case 'f':
+          stop();
+          break;
+      default:
+          break;
+    }
+  } 
+}
+void backward(){
+  digitalWrite(5,HIGH);
+  digitalWrite(6,LOW);
+  digitalWrite(9,HIGH);
+  digitalWrite(10,LOW);
+}
+void forward(){
+  digitalWrite(5,LOW);
+    digitalWrite(6,HIGH);
+    digitalWrite(9,LOW);
+    digitalWrite(10,HIGH);
+}
+void right(){
+  digitalWrite(5,LOW);
+    digitalWrite(6,HIGH);
+    digitalWrite(9,HIGH);
+    digitalWrite(10,LOW);
+
+}
+void left(){
+  digitalWrite(5,HIGH);
+    digitalWrite(6,LOW);
+    digitalWrite(9,LOW);
+    digitalWrite(10,HIGH);
+}
+void stop(){
+  digitalWrite(5,HIGH);
+    digitalWrite(6,HIGH);
+    digitalWrite(9,HIGH);
+    digitalWrite(10,HIGH);
+}
